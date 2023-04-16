84e53180b9385051271ebe2a343e74fe6702677a
Piarre <pierre@ideestore.fr>
2023-04-16 18:30:30 +0200

    Added a setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ ! -d "$HOME/.gitools" ]; then
+  mkdir "$HOME/.gitools"
+fi
+
+cd "$HOME/.gitools"
+curl -k https://raw.githubusercontent.com/Piarre/GiTools/main/gitools -o gitools
+if [ ! -f ~/.bashrc ]; then
+  touch ~/.bashrc
+fi
+echo 'Adding GiTools to PATH'
+echo '' >>~/.bashrc
+echo '# GiTools' >>~/.bashrc
+echo 'export PATH=$PATH:"$HOME/.gitools"' >>~/.bashrc
+echo 'GiTools added, please restart your terminal.'
