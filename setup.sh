1646b43ea0d16249712142a84492606bfb58521f
Pierre <pierre@ideestore.fr>
2023-04-18 22:26:24 +0200

    Update setup.sh
    Wasn't checking for the latest release

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+LATEST_RELEASE=$(curl -ks https://api.github.com/repos/Piarre/GiTools/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 if [ ! -d "$HOME/.gitools" ]; then
   mkdir "$HOME/.gitools"
 fi
-
 cd "$HOME/.gitools"
-curl -LJk https://github.com/Piarre/GiTools/releases/download/v1.0.0/gitools -o gitools
+curl -LJk https://github.com/Piarre/GiTools/releases/download/$LATEST_RELEASE/gitools -o gitools
 if [ ! -f ~/.bashrc ]; then
   touch ~/.bashrc
 fi
