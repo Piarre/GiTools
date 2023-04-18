bd466c94d7e09837605fefb53cbb5f20af78dbf3
Pierre <pierre@ideestore.fr>
2023-04-18 22:24:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,8 @@ if [ ! -d "$HOME/.gitools" ]; then
   mkdir "$HOME/.gitools"
 fi
 
-# shellcheck disable=SC2164
 cd "$HOME/.gitools"
-
-# Check if we use curl or wget
-if [ -x "$(command -v curl)" ]; then
-  curl -LJk https://github.com/Piarre/GiTools/releases/download/v1.0.0/gitools -o gitools
-elif [ -x "$(command -v wget)" ]; then
-  wget --no-check-certificate --content-disposition https://github.com/Piarre/GiTools/releases/download/v1.0.0/gitools
-# shellcheck disable=SC1073
-if
+curl -LJk https://github.com/Piarre/GiTools/releases/download/v1.0.0/gitools -o gitools
 if [ ! -f ~/.bashrc ]; then
   touch ~/.bashrc
 fi
